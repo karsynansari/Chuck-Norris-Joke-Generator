30037b7fb14ca52e947e0be69c929bc0b21baf20
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-24 16:40:25 -0700

    added json

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("hello")
+console.log("hello");
+
+fetch("http://api.icndb.com/jokes/random?firstName=John&lastName=Doe")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
