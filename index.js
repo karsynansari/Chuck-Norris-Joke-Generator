b4e5601c25b74fc1d1582fbb4bb4265dcaba201b
alexandra <alexandraschurman@gmail.com>
2022-01-25 09:26:38 -0800

    created post el

--- index.js
+++ index.js
@@ -11,18 +11,36 @@ fetch(JSON_URL)
 
 function renderJoke(JokeArr) {
     JokeArr.forEach(joke)
+
     }
 
 function joke(jokeObj) {
   console.log(jokeObj)
+
+
+
+  const postEl = document.createElement('p')
+  const postJoke = document.getElementById('post-block')
+postJoke.appendChild(postEl)
+
+// const qouteID = Math.floor(Math.random()* joke.length)
+
+// for (joke in jokeObj) {
+//   console.log(`${joke}: ${jokeObj[1]}`)
+
+// joke = object.value = > whatve the joke is 
+
 }
 
-getJoke();
+
+// getJoke();
+
+
+//   //gets id #
 
 
 
 
 
-      // const randomQoute = Math.floor(Math.random()* data.length)
-      // console.log(typeof randomQoute)
+
 
