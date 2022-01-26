18668b8c018b2f1b6f144a6dd0711c5761fd3c85
alexandra <alexandraschurman@gmail.com>
2022-01-26 09:54:46 -0800

    combed through code a bit more, added class to likebtn2

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// console.log("hello");
+
 //DOM Selectors
 
 const randomJokeDiv = document.querySelector("#post-block");
 const jokeButton = document.querySelector("#chuck-btn");
 const jokeSubmitForm = document.querySelector("#add-joke");
 console.log(jokeSubmitForm);
-// console.log(randomJokeDiv)
+
 
 //moved these 2 variables into the global scope from their original place inside the joke button event listener.
 //this made a new joke appear every time btn is clicked, instead of them all listing out, which is optimal imo.
 let generatedJoke = document.createElement("p");
 let likeBtn = document.createElement("button");
+likeBtn.className = 'likebtn'
 
 const JSON_URL = "http://localhost:3000/value";
 
-//   const postEl = document.createElement('p')
-//   const postJoke = document.getElementById('post-block')
-// postJoke.appendChild(postEl)
 
 function getJoke() {
   fetch(JSON_URL)
@@ -25,14 +23,12 @@ function getJoke() {
 }
 
 function renderJoke(JokeArr) {
-  // console.log(JokeArr)
   let newJokeArray = JokeArr.map((jokeObject) => {
-    // console.log(jokeObject.joke);
     return jokeObject.joke;
     // const values = Object.values(jokeObject)
-
-    // console.log(prop);
   });
+
+
   jokeButton.addEventListener("click", () => {
     let randomJoke =
       newJokeArray[Math.floor(Math.random() * newJokeArray.length)];
@@ -42,7 +38,6 @@ function renderJoke(JokeArr) {
     //just an example here, added some innner text just to makie it look decent
     likeBtn.innerHTML = "  Like";
     generatedJoke.textContent = randomJoke;
-    // console.log(generatedJoke)
     //append the new element to the parent div declared in DOM selectors
     randomJokeDiv.appendChild(generatedJoke);
     //appended the like button element to the joke
@@ -50,11 +45,16 @@ function renderJoke(JokeArr) {
     //tried creating an even listener to render the jokes to the DOM. Stopped here.
   });
 }
+
+
 //Event Listeners
+
+
 //Event listener for like button. We can add more sophisticated functionality to it, but for now it's working!
 likeBtn.addEventListener("click", () => {
   likeBtn.textContent = "Liked!";
 });
+
 //event listener for joke submit form
 jokeSubmitForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -68,9 +68,8 @@ function addNewJoke(userAddedJoke) {
   // console.log(userAddedJoke);
   let newUserJoke = document.createElement("p");
   let likeBtn2 = document.createElement("button");
-  // console.log(likeBtn2)
+  likeBtn2.className = 'likebtn'
   newUserJoke.textContent = userAddedJoke;
-  // console.log(newUserJoke);
   document.querySelector("#user-joke").appendChild(newUserJoke);
   newUserJoke.appendChild(likeBtn2)
   jokeSubmitForm.reset() 
